45fbb8bbe8cb57653847ef6fc4153fd4d506fdec
Lewis <harris@Lewiss-iMac.local>
2020-05-20 11:45:53 +0100

    gh-pages-build

---
+++ build/precache-manifest.989c4af27904ab371074063d02df3a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873f2f39aeaa082e6653c61235f147b5",
+    "url": "/React-Nutrition-Checker/index.html"
+  },
+  {
+    "revision": "e87c0e352e748c4d1669",
+    "url": "/React-Nutrition-Checker/static/css/main.14051972.chunk.css"
+  },
+  {
+    "revision": "b894079f988ed9966c49",
+    "url": "/React-Nutrition-Checker/static/js/2.0cff22fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Nutrition-Checker/static/js/2.0cff22fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87c0e352e748c4d1669",
+    "url": "/React-Nutrition-Checker/static/js/main.c9a45b26.chunk.js"
+  },
+  {
+    "revision": "122b6a3069f468b6e5a3",
+    "url": "/React-Nutrition-Checker/static/js/runtime-main.9ad22809.js"
+  }
+]);
