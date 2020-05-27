023021427930dad19870dc477172b7b08b6acfcc
Lewis <harris@Lewiss-iMac.local>
2020-05-27 12:51:02 +0100

    Updates

---
+++ precache-manifest.69753d9d7851eff8be79e392bfcd7c97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8724306f054920596d7e30f52636cf27",
+    "url": "/React-Nutrition-Checker/index.html"
+  },
+  {
+    "revision": "3d0a8ffe580dc8867f12",
+    "url": "/React-Nutrition-Checker/static/css/main.bbfc0ca2.chunk.css"
+  },
+  {
+    "revision": "6c95f9cc839bca8e3a5b",
+    "url": "/React-Nutrition-Checker/static/js/2.ef5a6119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Nutrition-Checker/static/js/2.ef5a6119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0a8ffe580dc8867f12",
+    "url": "/React-Nutrition-Checker/static/js/main.9a999079.chunk.js"
+  },
+  {
+    "revision": "122b6a3069f468b6e5a3",
+    "url": "/React-Nutrition-Checker/static/js/runtime-main.9ad22809.js"
+  },
+  {
+    "revision": "87ddb6881820d49d56baccbf509439ef",
+    "url": "/React-Nutrition-Checker/static/media/pork.87ddb688.jpeg"
+  }
+]);
